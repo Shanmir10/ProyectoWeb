b2c8bd3171b240f873adc02781920a4d01ce9192
JAV0 <javicho201410mail.com>
2024-04-28 21:43:10 -0500

    Modificacion de Inicio prt1
    se agrego el funcionamiento de incio falta agregar texto y conexiones, mas tarde lo hago

---
+++ scriptInicio.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const header = document.querySelector('header');
+    const container = document.getElementById('container');
+    const menuButton = document.getElementById('menu');
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    // funcion para manejar el desplazamiento
+    function handleScroll(){
+        container.classList.remove('menuopen');
+        header.classList.toggle('sticky', window.scrollY >= 100);
+    }
+
+    //funcion para manejar el cick del boton menu
+    function handleMenuButtonClick(){
+        header.classList.remove('sticky');
+        container.classList.toggle('menuopen');
+    }
+
+    //funcion para manejar el anchor de los links
+    function handleLinkClick(event){
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if(targetElement){
+            targetElement,scrollIntoView({
+                behavior:'smooth'
+            });
+        }
+    }
+
+    // funcion para cerrar el menu cuando clickeas y se muestra el menu pequeño
+    function handleCloseOutside(event){
+        if(!menuButton.contains(event.target)){
+            // verifiar si el click fue fuera del botn del menu
+            container.classList.remove('menuopen');
+            header.classList.add('sticky');
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    menuButton.addEventListener('click',handleMenuButtonClick);
+    links.forEach(link => link.addEventListener('click', handleCloseOutside));
+
+    document.addEventListener('click', handleCloseOutside);
+});
