80e74107591745d6e7eee06a04c5528d2b3ee1ae
JAV0 <javicho201410mail.com>
2024-04-28 22:39:39 -0500

    cambios en funcionamiento

--- scriptInicio.js
+++ scriptInicio.js
@@ -1,45 +1,46 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('header');
     const container = document.getElementById('container');
     const menuButton = document.getElementById('menu');
     const links = document.querySelectorAll('a[href^="#"]');
 
     // funcion para manejar el desplazamiento
-    function handleScroll(){
+    function handleScroll() {
         container.classList.remove('menuopen');
         header.classList.toggle('sticky', window.scrollY >= 100);
     }
 
     //funcion para manejar el cick del boton menu
-    function handleMenuButtonClick(){
+    function handleMenuButtonClick() {
         header.classList.remove('sticky');
         container.classList.toggle('menuopen');
     }
 
     //funcion para manejar el anchor de los links
-    function handleLinkClick(event){
+    function handleLinkClick(event) {
         event.preventDefault();
         const targetId = this.getAttribute('href');
         const targetElement = document.querySelector(targetId);
-        if(targetElement){
-            targetElement,scrollIntoView({
-                behavior:'smooth'
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
             });
         }
     }
 
     // funcion para cerrar el menu cuando clickeas y se muestra el menu pequeño
-    function handleCloseOutside(event){
-        if(!menuButton.contains(event.target)){
-            // verifiar si el click fue fuera del botn del menu
+    function handleCloseOutside(event) {
+        if (!menuButton.contains(event.target)) {
+            // verifiar si el click fue fuera del boton del menu
             container.classList.remove('menuopen');
             header.classList.add('sticky');
         }
     }
 
     window.addEventListener('scroll', handleScroll);
-    menuButton.addEventListener('click',handleMenuButtonClick);
-    links.forEach(link => link.addEventListener('click', handleCloseOutside));
+    menuButton.addEventListener('click', handleMenuButtonClick);
+    links.forEach(link => link.addEventListener('click', handleLinkClick));
 
+    //cualquier click en el documento
     document.addEventListener('click', handleCloseOutside);
 });
